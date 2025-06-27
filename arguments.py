3eed10a06faf27b7a06a8e22b9f5f61e6013f8a2
GabbaTK <109983404+GabbaTK@users.noreply.github.com>
2025-06-27 14:20:15 +0200

    Update arguments.py

--- arguments.py
+++ arguments.py
@@ -14,14 +14,14 @@ class ArgumentOrder:
 
 class Option:
     def __init__(self, argument: str | list[str], code_name: str, arg_type, help: str, default = None):
-        """
-        Creates a new option for the program
+        """Creates a new option for the program
 
-        argument: The argument/aliases for search for (-v, --verbose...)
-        code_name: The variable name to refrence in your code
-        arg_type: What type is the argument (str, int), can be None to specify an argument without parameters
-        help: When -h or --help is used, what should be displayed for this argument
-        default: What is the default value if its not provided (leave blank to make this argument mandatory)
+        Args:
+            argument (str | list[str]): The argument/aliases to search for
+            code_name (str): The variable name to refrence in your code
+            arg_type (_type_): The type of the argument. Can be None to specify an argument without parameters
+            help (str): When -h is used, what should be displayed for this argument
+            default (_type_, optional): The default value if its not provided. Leave blank to make this mandatory. Defaults to None.
         """
 
         if type(argument) == str:
@@ -36,17 +36,17 @@ class Option:
         self.default = default
         self.help = help
         self.type = ArgumentOrder.Options
-    
+
 class Positional:
     def __init__(self, argument: str, code_name: str, position: int, arg_type, default = None):
-        """
-        Creates a new option for the program
+        """Creates a new option for the program
 
-        argument: The argument displayed in help (your_program.py [OPTIONS] POSITIONAL-ARGUMENT)
-        code_name: The variable name to refrence in your code
-        position: In what position will the argument be provided (python your_program.py POSITIONAL-ARGUMNET-POSITION-1, POSITIONAL-ARGUMENT-POSITION-2)
-        arg_type: What type is the argument (str, int)
-        default: What is the default value if its not provided (leave blank to make this argument mandatory)
+        Args:
+            argument (str): The argument displayed in the help menu
+            code_name (str): The variable name to refrence in your code
+            position (int): In what position will the argument be provided
+            arg_type (_type_): What type is the argument
+            default (_type_, optional): What is the default value if its not provided. Leave blank to make this argument mandatory. Defaults to None.
         """
 
         self.code_name = code_name
@@ -58,18 +58,16 @@ class Positional:
 
 class ArgumentHandler:
     def __init__(self, description: str):
-        """
-        Creates a handler for script arguments
+        """Creates a handler for the arguments
 
-        description: A description for the program that is displayed once the -h argument is used or invalid arguments are passed
+        Args:
+            description (str): The program description to be passed when -h is used or an invalid argument is passed
         """
 
         self.description = description
         self.arguments = []
 
-    def generateHelp(self, root_name):
-        print(f"python {root_name}", end="")
-
+    def generateHelp(self):
         for arg in self.arguments:
             if arg.type == ArgumentOrder.Options:
                 print(" [OPTIONS]", end="")
@@ -132,32 +130,29 @@ class ArgumentHandler:
 
         exit()
 
-    def add(self, arg):
-        """
-        Adds an argument
+    def add(self, arg: Positional|Option):
+        """Adds an argument to the parsable arguments
 
-        arg: The argument (Positional or Option)
+        Args:
+            arg (Positional | Option): The argument
         """
 
         self.arguments.append(arg)
 
-    def parse(self, root_name: str):
-        """
-        Parses all the arguments provided
+    def parse(self) -> dict:
+        """Parses all the arguments
 
-        root_name: The name of the main program running, if you only use it, it will be __file__
+        Raises:
+            WrongArgumentType: A argument was provided with another type rather than the expected type
+
+        Returns:
+            dict: The parsed data
         """
 
         if "-h" in argv or "--help" in argv:
-            self.generateHelp(root_name)
-
-        try:
-            ignore_index = argv.index(root_name)
-        except ValueError:
-            #raise RootNameNotFound("The provided root name was incorrent")
-            ignore_index = 0
+            self.generateHelp()
         
-        args = argv[ignore_index + 1:]
+        args = argv[1:]
 
         arguments = {}
         for arg in self.arguments:
@@ -223,6 +218,6 @@ class ArgumentHandler:
             if arguments[arg.code_name] == None:
                 if arg.default == None:
                     # Argument is missing
-                    self.generateHelp(root_name)
+                    self.generateHelp()
 
         return arguments
